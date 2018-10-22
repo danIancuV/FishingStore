2575a3a0319d1fbc35700e619c99ef4da4fef809
danIancuV <33359205+danIancuV@users.noreply.github.com>
2018-10-22 21:36:36 +0300

    Add project files.

---
+++ FishingStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FishingStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
